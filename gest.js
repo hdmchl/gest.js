3742be0dfcfee7f89c15db2d52ad55d17b910d21
Hadi Michael Salem <hadisalem7@gmail.com>
2013-07-16 22:40:46 +1000

    start refactoring

--- gest.js
+++ gest.js
@@ -1,72 +1,221 @@
 window.gest = (function () {
-    gest = function () {
-    	//init
-        console.log("1");
-        this.myPublicProperty = "Asdf"
-    };
+	var framerate = 25;
 
-    var privateVariable = 123;
+	var video, canvas, context, ccanvas, ccontext;
+
+    gest = function () {
+		//create the required DOM elements
+		video = document.createElement('video');
+		video.id = "video";
+		video.width = 300;
+		video.setAttribute('autoplay', 'autoplay');
+		document.body.appendChild(video);
+
+		canvas = document.createElement('canvas');
+		canvas.id = "canvas";
+		canvas.style.width = "300px";
+		document.body.appendChild(canvas);
+
+		context = canvas.getContext('2d');
+
+		ccanvas = document.createElement('canvas');
+		ccanvas.setAttribute('style', "position: fixed; left: 0; top: 0; width: 100%; height: 100%; opacity: 1;");
+		document.body.appendChild(ccanvas);
+
+		ccontext = ccanvas.getContext('2d');
+
+    };
 
     var privateMethod = function () {
     	console.log("2");
     };
 
-    gest.prototype.publicMethod = function () {
-    	console.log("3");
+    gest.prototype.start = function () {
+    	window.URL = window.URL || window.webkitURL;
+		navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia;
+
+    	if (navigator.getUserMedia) {
+		  	navigator.getUserMedia(
+		  		{
+		  			audio: false,
+		  			video: true
+		  		}, function(stream) {
+		    		video.src = window.URL.createObjectURL(stream);
+
+		    		video.addEventListener('play',
+						function(){
+							width = Math.floor(video.videoWidth / compressionRate);
+							height = Math.floor(video.videoHeight / compressionRate);
+							setInterval(grabVideoFrame, 1000/framerate);
+						}
+					)
+		  		}, function() {
+		  			console.log('DEEEEENIED!');
+		  		});
+		} else {
+		  	console.log('getUserMedia is not supported!');
+		  	//video.src = 'somevideo.webm'; // fallback.
+		}
     };
+
+
+    /************/
+    var compressionRate = 5;
+	var width = height = 300;
+
+	function grabVideoFrame(){
+		//TODO: need to reset the canvas sizes, or else they don't redraw
+		canvas.width = ccanvas.width = width;
+		canvas.height = ccanvas.height = height;
+
+		//draw mirrored frame into context
+		context.drawImage(video, width, 0, -width, height);
+		
+		//copy the context into our processing context
+		var currentFrame = context.getImageData(0, 0, width, height);
+		ccontext.putImageData(currentFrame, 0, 0)
+		
+		//TODO: skinfilter()
+		checkForGestures(currentFrame)
+	}
+
+	var last 	= false;
+	var thresh 	= 150;
+	var down	= false;
+	var wasdown = false;
+
+	function checkForGestures(currentFrame){
+		delt = context.createImageData(width, height);
+
+		if (last !== false) {
+
+			var totalx 	= 0,
+				totaly 	= 0,
+				totald 	= 0,
+				totaln 	= delt.width * delt.height,
+				dscl 	= 0,
+				pix 	= totaln * 4;
+
+			while (pix -= 4) {
+
+				var d = Math.abs(currentFrame.data[pix] - last.data[pix]) + 
+						Math.abs(currentFrame.data[pix+1] - last.data[pix+1]) + 
+						Math.abs(currentFrame.data[pix+2] - last.data[pix+2]);
+
+				if (d > thresh) {
+					delt.data[pix] 		= 0; //R
+					delt.data[pix+1] 	= 0; //G
+					delt.data[pix+2] 	= 0; //B
+					delt.data[pix+3] 	= 255; //alpha
+					totald += 1;
+					totalx += ((pix/4) % width);
+					totaly += (Math.floor((pix/4) / delt.height));
+				}
+				else {
+					delt.data[pix] 		= 0; //R
+					delt.data[pix+1] 	= 0; //G
+					delt.data[pix+2] 	= 0; //B
+					delt.data[pix+3] 	= 0; //alpha
+				}
+
+			}
+		}
+
+		if (totald) {
+			//if some movement has been detected
+			var movement = {
+				x: totalx,
+				y: totaly,
+				d: totald
+			}
+
+			handle(movement);
+		}
+
+		//console.log(totald);
+		last = currentFrame;
+		ccontext.putImageData(delt, 0, 0);
+	}
+
+	movethresh=2
+	brightthresh=300
+	overthresh=1000
+	function calibrate(){
+		wasdown={
+			x:down.x,
+			y:down.y,
+			d:down.d
+		}
+	}
+	avg=0
+	state=0//States: 0 waiting for gesture, 1 waiting for next move after gesture, 2 waiting for gesture to end
+
+	function handle(movement){
+		down = {
+			x: movement.x / movement.d,
+			y: movement.y / movement.d,
+			d: movement.d,
+		};
+
+		avg=0.9*avg+0.1*down.d
+		var davg = down.d - avg,
+			good = davg > brightthresh;
+
+		//console.log(davg)
+		
+		switch(state){
+			case 0:
+				if(good){//Found a gesture, waiting for next move
+					state=1
+					calibrate();
+				}
+				break
+			case 2://Wait for gesture to end
+				if(!good){//Gesture ended
+					state=0
+				}
+				break;
+			case 1://Got next move, do something based on direction
+				var dx=down.x-wasdown.x,dy=down.y-wasdown.y
+				var dirx=Math.abs(dy)<Math.abs(dx)//(dx,dy) is on a bowtie
+				//console.log(good,davg)
+				if(dx<-movethresh&&dirx){
+					console.log('right');
+				}
+				else if(dx>movethresh&&dirx){
+					console.log('left');
+				}
+				if(dy>movethresh&&!dirx){
+					if(davg>overthresh){
+						console.log('long up');
+					}
+					else{
+						console.log('up');
+					}
+				}
+				else if(dy<-movethresh&&!dirx){
+					if(davg>overthresh){
+						console.log('long down')
+						//Reveal.toggleOverview()
+					}
+					else{
+						console.log('down');
+					}
+				}
+				state=2
+				break
+		}
+	}
+    /************/
 
     return new gest();
 }());
 
-console.log(gest.myPublicProperty);
+gest.start();
 
 /**********************/
 
-var video 	= document.getElementById('video');
-var canvas 	= document.getElementById('canvas');
-var _		= canvas.getContext('2d');
-var ccanvas	= document.getElementById('comp');
-var c_ 		= ccanvas.getContext('2d');
 
-navigator.webkitGetUserMedia(
-	{
-		audio:false,
-		video:true
-	}, function(stream){
-		video.src = window.webkitURL.createObjectURL(stream);
-
-		video.addEventListener('play',
-			function(){
-				var framerate = 25;
-				setInterval(dump, 1000/framerate);
-			}
-		)
-	}, function(){
-		console.log('OOOOOOOH! DEEEEENIED!')
-	}
-);
-
-var compression = 5;
-var width = 0;
-var height = 0;
-
-function dump(){
-	if (canvas.width != video.videoWidth){
-		width = Math.floor(video.videoWidth / compression);
-		height = Math.floor(video.videoHeight / compression);
-		canvas.width = ccanvas.width = width;
-		canvas.height = ccanvas.height = height;
-	}
-	
-	_.drawImage(video, width, 0, -width, height);
-	
-	draw = _.getImageData(0 , 0, width, height);
-	
-	//c_.putImageData(draw,0,0)
-	//skinfilter()
-	
-	test()
-}
 
 /*
 huemin=0.0
@@ -78,7 +227,7 @@ valmax=1.0
 
 function skinfilter(){
 	
-	skin_filter=_.getImageData(0,0,width,height)
+	skin_filter=context.getImageData(0,0,width,height)
 	var total_pixels=skin_filter.width*skin_filter.height
 	var index_value=total_pixels*4
 	
@@ -147,121 +296,4 @@ function rgb2Hsv(r, g, b){
 }
 */
 
-last 	= false;
-thresh 	= 150;
-down	= false;
-wasdown = false;
 
-function test(){
-	delt = _.createImageData(width, height);
-
-	if (last !== false) {
-
-		var totalx 	= 0,
-			totaly 	= 0,
-			totald 	= 0,
-			totaln 	= delt.width * delt.height,
-			dscl 	= 0,
-			pix 	= totaln * 4;
-
-		while (pix -= 4) {
-			var d = Math.abs(draw.data[pix] - last.data[pix]) + 
-					Math.abs(draw.data[pix+1] - last.data[pix+1]) + 
-					Math.abs(draw.data[pix+2] - last.data[pix+2]);
-
-			if (d > thresh) {
-				delt.data[pix] = 0
-				delt.data[pix+1] = 0
-				delt.data[pix+2] = 0
-				delt.data[pix+3] = 255
-				totald += 1
-				totalx += ((pix/4)%width)
-				totaly += (Math.floor((pix/4)/delt.height))
-			}
-			else{
-				delt.data[pix] = 0;
-				delt.data[pix+1] = 0;
-				delt.data[pix+2] = 0;
-				delt.data[pix+3] = 0;
-			}
-		}
-	}
-
-	if (totald) {
-		down = {
-			x: totalx/totald,
-			y: totaly/totald,
-			d: totald
-		}
-
-		handledown();
-	}
-
-	//console.log(totald)
-	last = draw;
-	c_.putImageData(delt, 0, 0)
-}
-
-movethresh=2
-brightthresh=300
-overthresh=1000
-function calibrate(){
-	wasdown={
-		x:down.x,
-		y:down.y,
-		d:down.d
-	}
-}
-avg=0
-state=0//States: 0 waiting for gesture, 1 waiting for next move after gesture, 2 waiting for gesture to end
-
-function handledown(){
-	avg=0.9*avg+0.1*down.d
-	var davg = down.d - avg,
-		good = davg > brightthresh;
-
-	//console.log(davg)
-	
-	switch(state){
-		case 0:
-			if(good){//Found a gesture, waiting for next move
-				state=1
-				calibrate();
-			}
-			break
-		case 2://Wait for gesture to end
-			if(!good){//Gesture ended
-				state=0
-			}
-			break;
-		case 1://Got next move, do something based on direction
-			var dx=down.x-wasdown.x,dy=down.y-wasdown.y
-			var dirx=Math.abs(dy)<Math.abs(dx)//(dx,dy) is on a bowtie
-			//console.log(good,davg)
-			if(dx<-movethresh&&dirx){
-				console.log('right');
-			}
-			else if(dx>movethresh&&dirx){
-				console.log('left');
-			}
-			if(dy>movethresh&&!dirx){
-				if(davg>overthresh){
-					console.log('long up');
-				}
-				else{
-					console.log('up');
-				}
-			}
-			else if(dy<-movethresh&&!dirx){
-				if(davg>overthresh){
-					console.log('long down')
-					//Reveal.toggleOverview()
-				}
-				else{
-					console.log('down');
-				}
-			}
-			state=2
-			break
-	}
-}
