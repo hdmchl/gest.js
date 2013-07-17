dd6b9bb0927971918c56c9b51842c9cf026508a0
Hadi Michel Salem <hsalem@deloitte.com.au>
2013-07-17 17:32:37 +1000

    add the DD carousel demo to git

---
+++ DD-demo.js
@@ -0,0 +1,28 @@
+// functioning demo on 17/07/2013
+
+var head = document.getElementsByTagName('head')[0];
+var script = document.createElement('script');
+script.type = 'text/javascript';
+script.src = 'https://bitbucket.org/hadimichael/gest.js/raw/c05a8313a0255564f22e63132e4fca1d4bfd0cd0/gest.js';
+
+script.onload = function() { 
+	var div = document.createElement('div');
+	div.id = "output";
+	div.setAttribute('style', 'position: fixed; top: 10px; left: 10px; width: 100px; z-index:100; color: lime; font-size: 35px; font-family: Arial;');
+	document.body.appendChild(div);
+	
+	document.addEventListener('gestRecognised', function(gesture) {
+		document.getElementById('output').innerHTML = gesture.direction;
+		if (gesture.direction === 'left') {
+			$('.next').trigger('click');
+		} else if (gesture.direction === 'right') {
+			$('.prev').trigger('click');
+		}
+	}, false);
+
+	gest.start();
+
+	document.getElementById('output').innerHTML = 'loaded';
+};
+
+head.appendChild(script);
